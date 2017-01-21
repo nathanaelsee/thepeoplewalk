92fad72ab5d3df79c13ee13d3f9a6c70d885c189
Michael Liu <mike666234@gmail.com>
2017-01-21 04:28:38 -0800

    frontend first commit

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+/*app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});*/
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
